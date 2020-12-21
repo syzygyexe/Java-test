4a5d2370854878c3913855a71469ee9e79aec36a
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-21 16:40:14 +0900

    initial

---
+++ 3/CharAndBoolean.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myUnicodeCopyright = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myUnicodeCopyright);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
