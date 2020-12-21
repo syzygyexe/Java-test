8d6679937cfea1fbca61a75aca702f851986aa99
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-21 16:47:01 +0900

    initial

---
+++ 3/ByteShortIntLong.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+        System.out.println(myValue);
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value is equal to " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value is equal to " + (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum value = " + myMinByteValue);
+        System.out.println("Byte Maximum value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum value = " + myMinShortValue);
+        System.out.println("Short Maximum value = " + myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum value = " + myMinLongValue);
+        System.out.println("Long Maximum value = " + myMaxLongValue);
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+    }
+}
