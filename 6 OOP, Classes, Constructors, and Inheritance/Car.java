d6b1efc25c49b5e3923eb2f247c0c4ac588781b5
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-23 20:33:38 +0900

    initial

---
+++ 6 OOP, Classes, Constructors, and Inheritance/Car.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        // Only if the passed value is equal to carrera or holden we will allow
+        // our class to change the value.
+        if(validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
