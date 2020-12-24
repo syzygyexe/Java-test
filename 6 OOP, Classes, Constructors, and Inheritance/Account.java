1d0bfd757b5ce3097d2a5d8d7a23fffd6cee7c24
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-24 22:17:44 +0900

    initial

---
+++ 6 OOP, Classes, Constructors, and Inheritance/Account.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class Account {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String emailAddress;
+    private String customerPhoneNumber;
+
+    public Account() {
+        this("5679", 2.50, "Default name",
+                "Default address", "default phone");
+        System.out.println("Empty constructor called");
+    }
+
+    public Account(String number, double balance, String customerName, String emailAddress,
+                   String customerPhoneNumber) {
+        System.out.println("Account with parameters called");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.emailAddress = emailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if(this.balance - withdrawalAmount < 0) {
+            System.out.println("Only " + this.balance + " available. Withdrawal not processed");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance = " + this.balance);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+}
