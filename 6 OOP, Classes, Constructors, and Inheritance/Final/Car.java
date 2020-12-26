1cbfa49e5290f0d01b197e21c45d222183053a03
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-26 16:07:12 +0900

    initial

---
+++ 6 OOP, Classes, Constructors, and Inheritance/Final/Car.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Car extends Vehicle {
+
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        // We assume whenever we create a new Car, it's  gear is set to a 1st gear.
+        this.currentGear = 1;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + currentGear + " gear");
+    }
+
+    public void changeVelocity(int speed, int direction) {
+        System.out.println("Car.changeVelocity() : Velocity " + speed + " direction " + direction);
+        move(speed, direction);
+    }
+}
